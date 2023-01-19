e4af59e0a5c85f459214801f009a4da796a0a0b7
Andres Molano <andresfms1@hotmail.com>
2023-01-19 09:37:04 -0500

    feat:se implementa consumo del cliente a controlador inversion e issue 666

--- controllers/inversion.go
+++ controllers/inversion.go
@@ -22,7 +22,9 @@ type InversionController struct {
 // URLMapping ...
 func (c *InversionController) URLMapping() {
 	c.Mapping("AddProyecto", c.AddProyecto)
+	c.Mapping("GuardarDocumentos", c.GuardarDocumentos)
 	c.Mapping("GetProyectoId", c.GetProyectoId)
+	c.Mapping("GetAllProyectos", c.GetAllProyectos)
 	c.Mapping("ActualizarSubgrupoDetalle", c.ActualizarSubgrupoDetalle)
 	c.Mapping("ActualizarProyectoGeneral", c.ActualizarProyectoGeneral)
 	// c.Mapping("GetActividadesGenerales", c.GetActividadesGenerales)
@@ -49,13 +51,12 @@ func (c *InversionController) AddProyecto() {
 	clienteHttp := &http.Client{}
 	var idPlan map[string]interface{}
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &registroProyecto); err == nil {
-		fmt.Println(registroProyecto)
 		plan["activo"] = true
 		plan["nombre"] = registroProyecto["nombre_proyecto"]
 		plan["descripcion"] = registroProyecto["codigo_proyecto"]
 		plan["tipo_plan_id"] = " "
 		plan["aplicativo_id"] = "idPlaneacion"
-		fmt.Println(plan)
+		//fmt.Println(plan)
 		aux, err := json.Marshal(plan)
 		if err != nil {
 			panic(err)
@@ -115,6 +116,43 @@ func (c *InversionController) AddProyecto() {
 
 }
 
+// GuardarDocumentos ...
+// @Title GuardarDocumentos
+// @Description post AddProyecto
+// @Param	body		body 	{}	true		"body for Plan content"
+// @Success 200
+// @router /guardar-documentos [post]
+func (c *InversionController) GuardarDocumentos() {
+	var body map[string]interface{}
+	var evidencias []map[string]interface{}
+	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &body); err == nil {
+		if body["documento"] != nil {
+			resDocs := helpers.GuardarDocumento(body["documento"].([]interface{}))
+
+			for _, doc := range resDocs {
+
+				evidencias = append(evidencias, map[string]interface{}{
+					"Id":     doc.(map[string]interface{})["Id"],
+					"Enlace": doc.(map[string]interface{})["Enlace"],
+					"nombre": doc.(map[string]interface{})["Nombre"],
+					"TipoDocumento": map[string]interface{}{
+						"id":                doc.(map[string]interface{})["TipoDocumento"].(map[string]interface{})["Id"],
+						"codigoAbreviacion": doc.(map[string]interface{})["TipoDocumento"].(map[string]interface{})["CodigoAbreviacion"],
+					},
+					"Observacion": "",
+					"Activo":      true,
+				})
+			}
+			c.Data["json"] = map[string]interface{}{"Success": true, "Status": "200", "Message": "Successful", "Data": evidencias}
+			c.ServeJSON()
+		} else {
+			c.Data["json"] = map[string]interface{}{"Code": "400", "Body": err, "Type": "error"}
+			c.Abort("400")
+		}
+	}
+
+}
+
 // GetProyectoId ...
 // @Title GetProyectoId
 // @Description get GetProyectoId
@@ -142,7 +180,6 @@ func (c *InversionController) GetProyectoId() {
 	var subgruposData map[string]interface{}
 	if err := request.GetJson("http://"+beego.AppConfig.String("PlanesService")+"/plan/"+id, &res); err == nil {
 		helpers.LimpiezaRespuestaRefactor(res, &infoProyect)
-		fmt.Println(infoProyect)
 		getProyect["nombre_proyecto"] = infoProyect["nombre"]
 		getProyect["codigo_proyecto"] = infoProyect["descripcion"]
 		getProyect["fecha_creacion"] = infoProyect["fecha_creacion"]
@@ -150,10 +187,7 @@ func (c *InversionController) GetProyectoId() {
 
 		var infoSubgrupos []map[string]interface{}
 		if err := request.GetJson("http://"+beego.AppConfig.String("PlanesService")+"/subgrupo?query=padre:"+padreId, &subgruposData); err == nil {
-			fmt.Println(subgruposData)
 			helpers.LimpiezaRespuestaRefactor(subgruposData, &infoSubgrupos)
-			//getProyect["fecha_creacion"]
-			fmt.Println(infoSubgrupos)
 			for i := range infoSubgrupos {
 				var subgrupoDetalle map[string]interface{}
 				var detalleSubgrupos []map[string]interface{}
@@ -184,6 +218,57 @@ func (c *InversionController) GetProyectoId() {
 					}
 				}
 			}
+		}
+		c.Data["json"] = map[string]interface{}{"Success": true, "Status": "200", "Message": "Successful", "Data": getProyect}
+	} else {
+		panic(map[string]interface{}{"funcion": "GetProyectoId", "err": "Error obteniendo información plan", "status": "400", "log": err})
+	}
+	c.ServeJSON()
+}
+
+// GetAllProyectos ...
+// @Title GetAllProyectos
+// @Description get GetProyectoId
+// @Param	aplicativo_id		path 	string	true		"The key for staticblock"
+// @Success 200
+// @router /getproyectos/:aplicativo_id [get]
+func (c *InversionController) GetAllProyectos() {
+	//fmt.Println("entró a la función")
+	// defer func() {
+	// 	if err := recover(); err != nil {
+	// 		localError := err.(map[string]interface{})
+	// 		c.Data["mesaage"] = (beego.AppConfig.String("appname") + "/" + "InversionController" + "/" + (localError["funcion"]).(string))
+	// 		c.Data["data"] = (localError["err"])
+	// 		if status, ok := localError["status"]; ok {
+	// 			c.Abort(status.(string))
+	// 		} else {
+	// 			c.Abort("404")
+	// 		}
+	// 	}
+	// }()
+	aplicativo_id := c.Ctx.Input.Param(":aplicativo_id")
+
+	var res map[string]interface{}
+	var getProyect []map[string]interface{}
+	//infoProyect := make(map[string]interface{})
+	var proyecto map[string]interface{}
+	var dataProyects []map[string]interface{}
+	//var subgrupoDetalle map[string]interface{}
+	//var fuentes map[string]interface{}
+
+	proyect := make(map[string]interface{})
+	//var subgruposData map[string]interface{}
+	if err := request.GetJson("http://"+beego.AppConfig.String("PlanesService")+"/plan/?query=activo:true,aplicativo_id:"+aplicativo_id, &res); err == nil {
+		helpers.LimpiezaRespuestaRefactor(res, &dataProyects)
+		//fmt.Println(res, "consulta realizada")
+		for i := range dataProyects {
+			if dataProyects[i]["activo"] == true {
+				proyect["id"] = dataProyects[i]["_id"]
+				//padreId := dataProyects[i]["_id"].(string)
+				proyecto = inversionhelper.GetDataProyects(dataProyects[i]["_id"].(string))
+			}
+			getProyect = append(getProyect, proyecto)
+
 		}
 		c.Data["json"] = map[string]interface{}{"Success": true, "Status": "200", "Message": "Successful", "Data": getProyect}
 	} else {
