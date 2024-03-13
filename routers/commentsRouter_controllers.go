584b20d61980612ef4f647e15bc971d70ee6ff44
ccmendezt <ccmendezt@udistrital.edu.co>
2024-03-13 14:55:19 -0500

    feat: se agregó la creación de registros de seguimiento para planes con nueva estructura

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -198,15 +198,6 @@ func init() {
 
     beego.GlobalControllerRouter["github.com/udistrital/planeacion_mid/controllers:FormulacionController"] = append(beego.GlobalControllerRouter["github.com/udistrital/planeacion_mid/controllers:FormulacionController"],
         beego.ControllerComments{
-            Method: "GetPlanesUnidadesComun",
-            Router: "/get_planes_unidades_comun/:id",
-            AllowHTTPMethods: []string{"post"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
-
-    beego.GlobalControllerRouter["github.com/udistrital/planeacion_mid/controllers:FormulacionController"] = append(beego.GlobalControllerRouter["github.com/udistrital/planeacion_mid/controllers:FormulacionController"],
-        beego.ControllerComments{
             Method: "GetRubros",
             Router: "/get_rubros",
             AllowHTTPMethods: []string{"get"},
