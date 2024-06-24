1d46cc54e292fbe2547a0a1bf05d58f2070cced4
felimarod <felipemartinrod@gmail.com>
2024-06-23 21:16:55 -0500

    feat: avance en test unitarios https://github.com/udistrital/planeacion_cliente/issues/1148

--- tests/controllers/arbol_test.go
+++ tests/controllers/arbol_test.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"bytes"
 	"net/http"
 	"testing"
 )
@@ -15,6 +16,46 @@ func TestGetObtenerArbol(t *testing.T) {
 		}
 	} else {
 		t.Error("Error TestGetObtenerArbol:", err.Error())
+		t.Fail()
+	}
+}
+
+// TODO: falta armar petición con :id y JSON
+func TestActivarPlan(t *testing.T) {
+	body := []byte(`{}`)
+
+	if request, err := http.NewRequest(http.MethodPut, "http://localhost:8081/v1/arbol/activar_plan/:id", bytes.NewBuffer(body)); err == nil {
+		client := &http.Client{}
+		if response, err := client.Do(request); err == nil {
+			if response.StatusCode != 200 {
+				t.Error("Error TestActivarPlan Se esperaba 200 y se obtuvo", response.StatusCode)
+				t.Fail()
+			} else {
+				t.Log("TestActivarPlan Finalizado Correctamente (OK)")
+			}
+		}
+	} else {
+		t.Error("Error al crear la solicitud PUT: ", err.Error())
+		t.Fail()
+	}
+}
+
+// TODO: falta armar petición con :id y JSON
+func TestActivarNodo(t *testing.T) {
+	body := []byte(`{}`)
+
+	if request, err := http.NewRequest(http.MethodPut, "http://localhost:8081/v1/arbol/activar_nodo", bytes.NewBuffer(body)); err == nil {
+		client := &http.Client{}
+		if response, err := client.Do(request); err == nil {
+			if response.StatusCode != 200 {
+				t.Error("Error TestActivarNodo Se esperaba 200 y se obtuvo", response.StatusCode)
+				t.Fail()
+			} else {
+				t.Log("TestActivarNodo Finalizado Correctamente (OK)")
+			}
+		}
+	} else {
+		t.Error("Error al crear la solicitud PUT: ", err.Error())
 		t.Fail()
 	}
 }
